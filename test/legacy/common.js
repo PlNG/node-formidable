c96b38d16faa382a68163b4e041555fde55b4324
PlNG <tempeml@hotmail.com>
2011-10-28 01:12:13 -0300

    Formatting for consistency.

--- test/legacy/common.js
+++ test/legacy/common.js
@@ -1,24 +1,16 @@
-var path = require('path'),
-    fs = require('fs');
-
+var path = require("path"),
+	fs = require("fs");
 try {
-  global.Gently = require('gently');
+	global.Gently = require("gently");
 } catch (e) {
-  throw new Error('this test suite requires node-gently');
+	throw new Error("this test suite requires node-gently");
 }
-
-exports.lib = path.join(__dirname, '../../lib');
-
+exports.lib = path.join(__dirname, "../../lib");
 global.GENTLY = new Gently();
-
-global.assert = require('assert');
+global.assert = require("assert");
 global.TEST_PORT = 13532;
-global.TEST_FIXTURES = path.join(__dirname, '../fixture');
-global.TEST_TMP = path.join(__dirname, '../tmp');
-
-// Stupid new feature in node that complains about gently attaching too many
-// listeners to process 'exit'. This is a workaround until I can think of a
-// better way to deal with this.
+global.TEST_FIXTURES = path.join(__dirname, "../fixture");
+global.TEST_TMP = path.join(__dirname, "../tmp");
 if (process.setMaxListeners) {
-  process.setMaxListeners(10000);
+	process.setMaxListeners(1E4);
 }
